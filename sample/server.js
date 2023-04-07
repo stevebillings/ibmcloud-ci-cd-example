5e19d706b1e61c57407305568768ae4c33393e98
sblimited <billings.steve@gmail.com>
2023-04-07 09:45:41 -0400

    changed response body

--- sample/server.js
+++ sample/server.js
@@ -2,7 +2,10 @@ const http = require('http');
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end("Yo yo yo!");
+
+   let dt = new Date();
+   response.end("This is a new string returned on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
+
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
